0de3e6745a9744b222d2dd222a364302c5d1cb7f
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-05-11 15:49:50 -0300

    107. Añadiendo método handler ver

--- src/main/java/com/sarracent/springboot/error/app/controllers/AppController.java
+++ src/main/java/com/sarracent/springboot/error/app/controllers/AppController.java
@@ -1,14 +1,30 @@
 package com.sarracent.springboot.error.app.controllers;
 
+import com.sarracent.springboot.error.app.controllers.models.domain.Usuario;
+import com.sarracent.springboot.error.app.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AppController {
+    @Autowired
+    private UsuarioService usuarioService;
+
     @GetMapping("/index")
     public String index() {
         Integer valor = 100/0;
         //Integer valor = Integer.parseInt("10x");
         return "index";
+    }
+
+    @GetMapping("/ver/{id}")
+    public String ver(@PathVariable Integer id, Model model) {
+        Usuario usuario = usuarioService.obtenerPorId(id);
+        model.addAttribute("usuario",usuario);
+        model.addAttribute("Titulo", "Detalle usuario: " .concat(usuario.getNombre()));
+        return "ver";
     }
 }
