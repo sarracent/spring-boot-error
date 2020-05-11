ace8ddd7bc6cbebdf3c1ffc6e97641a6b9199224
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-05-11 13:36:11 -0300

    104. Anotación @ControllerAdvice para manejo de excepciones

---
+++ src/main/java/com/sarracent/springboot/error/app/controllers/ErrorHandlerController.java
@@ -0,0 +1,20 @@
+package com.sarracent.springboot.error.app.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class ErrorHandlerController {
+    @ExceptionHandler(ArithmeticException.class)
+    public String aritmeticaError(Exception e, Model model) {
+        model.addAttribute("error", "Error de aritmetica");
+        model.addAttribute("message", e.getMessage());
+        model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        model.addAttribute("timestamp", new Date());
+        return "error/aritmetica";
+    }
+}
